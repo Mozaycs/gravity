17b8a31e11c22db8b56fc85792266e02525a3033
harsh5566 <harshav88@gmail.com>
2017-11-30 17:44:29 +0530

    header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+
+class Footer extends Component {
+  render() {
+    return (
+  <footer>
+  about us
+  </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+
+class Header extends Component {
+  render() {
+    return (
+    <div>
+    <Header>
+
+
+  </Header>
+
+    </div>
+    );
+  }
+}
+
+export default Header;
